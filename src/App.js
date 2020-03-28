03bb7515def1482e088a7e542e7c4d24a6b2addc
Rafael Laptop <rafael.jose1201@gmail.com>
2020-03-28 10:04:40 -0300

    Initial commit working on the rolled values display table

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Dice roller
       </header>
+      <div className="rolls-wrapper">
+        <div className="rolls-wrapper__header">
+          <div className="rolls-wrapper__header-item">
+            Player name
+          </div>
+          <div className="rolls-wrapper__header-item">
+            Roll type
+          </div>
+          <div className="rolls-wrapper__header-item">
+            Roll result
+          </div>
+          <div className="rolls-wrapper__header-item">
+            Time rolled
+          </div>
+        </div>
+        <div className="rolls-wrapper__body">
+          <div className="rolls-wrapper__body-item"></div>
+          <div className="rolls-wrapper__body-item"></div>
+          <div className="rolls-wrapper__body-item"></div>
+          <div className="rolls-wrapper__body-item"></div>  
+        </div>
+      </div>
     </div>
   );
 }
